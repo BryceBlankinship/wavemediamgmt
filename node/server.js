b210f8d38346513e541a9cedd3a06bb3109bdc2d
Bryce Blankinship <blankinshipbusiness@gmail.com>
2021-09-05 17:26:27 -0400

    create node test server for aws

---
+++ node/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the home page");
+});
+
+
+app.listen(3000, () => {
+    console.log("Server launch success");
+});
+

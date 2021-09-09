99463a7d18964acfbe39ea13f50016e28c4bafac
Bryce Blankinship <blankinshipbusiness@gmail.com>
2021-09-09 09:52:14 -0400

    make port const

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
     res.send("Welcome to the home page");
 });
 
-port = process.env.port || 8081;
+const port = process.env.port || 8081;
 app.listen(port, () => {
     console.log("Server launch success");
 });
